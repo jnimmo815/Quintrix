a4addc35cf2aaa9ae70d7ebb89c5b184ccb72e6e
James Nimmo <Tristan_Nimmo@msn.com>
2022-04-18 15:27:09 -0400

    assignent 3, first commit

---
+++ StreamDemo1/src/main/java/com/quintrix/jfs/models/Cat.java
@@ -0,0 +1,93 @@
+package com.quintrix.jfs.models;
+
+import java.util.Objects;
+
+public class Cat {
+
+	private Integer id;	
+	private String name;	
+	private Integer age;
+	private String color;
+	
+	
+	public Cat(Integer id, String name, Integer age, String color) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.color = color;
+	}
+
+
+	// Getter and Setters
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Integer getAge() {
+		return age;
+	}
+
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+
+	public String getColor() {
+		return color;
+	}
+
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+	// toString
+	@Override
+	public String toString() {
+		return "Cat [id=" + id + ", name=" + name + ", age=" + age + ", color=" + color + "]";
+	}
+
+
+	// hashCode and equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, color, id, name);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cat other = (Cat) obj;
+		return Objects.equals(age, other.age) && Objects.equals(color, other.color) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name);
+	}
+	
+	
+	
+	
+}
---
+++ StreamDemo1/src/main/java/com/quintrix/jfs/streamdemo/App.java
@@ -0,0 +1,44 @@
+package com.quintrix.jfs.streamdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.quintrix.jfs.models.Cat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       List<Cat> cats = new ArrayList<>();
+       
+       cats.add(new Cat(1, "Callie", 12, "Callico"));
+       cats.add(new Cat(2, "Einstien", 11, "Spotted"));
+       cats.add(new Cat(3, "Feynman", 5, "Tuxedo"));
+       cats.add(new Cat(4, "Maxwell", 12, "Spotted"));
+       cats.add(new Cat(5, "Morris", 12, "Orange"));
+       
+       Stream olderCats = cats.stream().filter(c -> c.getAge() > 10); // intermediate
+       
+       Stream vegStream = Stream.of("tomoto", "Green Chilli", "Pototo"); // intermediate
+       Stream sortedStream = vegStream.sorted();
+       sortedStream.forEach(name -> System.out.println(name));
+       
+       List<Cat> oldCats = cats.stream().filter(c -> c.getAge() > 10).collect(Collectors.toList()); // terminal      
+       System.out.println(oldCats);
+       
+       cats.stream().filter((c) -> c.getName().startsWith("E")).forEach(System.out :: println); // terminal    
+           
+      List<String> catNames = cats.stream().map(Cat::getName).collect(Collectors.toList());     // terminal
+      catNames.forEach(System.out :: println);
+      
+     catNames.stream().sorted().map(String::toUpperCase) // intermediate
+              .forEach(System.out::println);;
+       
+    }
+}
