5a5151c54bba337cffac2e27a3845bc890641e2c
James Nimmo <Tristan_Nimmo@msn.com>
2022-04-25 02:26:45 -0400

    first commit to Week5

---
+++ JPADemo/src/main/java/com/quintrix/james/JPADemo/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.quintrix.james.JPADemo.service;
+
+import java.util.List;
+
+import com.quintrix.james.JPADemo.entity.Employee;
+import com.quintrix.james.JPADemo.models.GetEmployeeResponse;
+
+
+public interface EmployeeService {
+	
+	Employee addEmployee(Employee employee);
+	
+	void deleteEmployeeById(Integer id);
+	
+	Employee getEmployeeById(Integer id);
+	
+	GetEmployeeResponse getEmployee(String lastName);	
+	
+	
+	// List<GetEmployeeResponse> getAllEmployees();
+
+}
---
+++ JPADemo/src/test/java/com/quintrix/james/JPADemo/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quintrix.james.JPADemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
